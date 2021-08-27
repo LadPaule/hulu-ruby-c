86a6cd6d97771a226bdc5977283a86a0b56a3268
LadPaule <ssozipaule@gmail.com>
2021-08-27 19:54:29 +0300

    change1

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 function footer() {
   return (
-    <div className="flex flex-col items-center bg-[#08222c]">
+    <div className="flex flex-col items-center bg-[#08222c] sm:font-extrabold">
       <p>This is developed using NextJs and Tailwind CSS</p>
       <a className="hover:text-white cursor-pointer hover:font-bold animate-bounce" href="https://github.com/LadPaule/hulu-Ruby">Follow the github repo</a>
       <a className="text-red-500 font-semibold" href="ssozipaule@gmail.com">ssozipaule@gmail.com</a>
