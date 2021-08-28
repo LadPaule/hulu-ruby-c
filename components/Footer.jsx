1a561f5a8de6ad7ba34ff58f354561c91f9fd461
LadPaule <ssozipaule@gmail.com>
2021-08-28 12:45:19 +0300

    changed git hub link

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ function footer() {
   return (
     <div className="flex flex-col items-center bg-[#08222c] sm:font-extrabold">
       <p>This is developed using NextJs and Tailwind CSS</p>
-      <a className="hover:text-white cursor-pointer hover:font-bold animate-bounce" href="https://github.com/LadPaule/hulu-Ruby">Follow the github repo</a>
+      <a className="hover:text-white cursor-pointer hover:font-bold animate-bounce" href="https://github.com/LadPaule/hulu-ruby-c">Follow the github repo</a>
       <a className="text-red-500 font-semibold" href="ssozipaule@gmail.com">ssozipaule@gmail.com</a>
         
       <div>
